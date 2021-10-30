28c4c8f7e2c26863757b73964a3386a1de4ca648
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-10-30 12:22:57 +0530

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose');
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+const port = 5000;
+var apiRouter = require("./routes/api");
+
+app.use(apiRouter)
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+
+var mongoUrl = 'mongodb://127.0.0.1/instagram'
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+
+app.listen(port, ()=>{
+    console.log(`app listening at http://localhost:${port}`)
+})
